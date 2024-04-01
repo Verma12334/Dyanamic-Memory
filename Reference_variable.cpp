35943787f5e6c4fc4316b6157060b03570d817c5
Saurabh <165586434+Verma12334@users.noreply.github.com>
2024-04-01 15:37:05 +0530

    Add files via upload

---
+++ Reference_variable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=5;
+    //Creating reference variable
+    int&j=i;
+    cout<<i<<endl;
+    cout<<j<<endl;
+    i++;
+    cout<<i<<endl;
+    cout<<j<<endl;
+    j++;
+    cout<<i<<endl;
+    cout<<j<<endl;
+}
